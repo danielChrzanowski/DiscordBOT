47d129e7ffd08c0bbcc8f0db6875034370d66a7b
Daniel <daniel_chrzanowski98@wp.pl>
2021-03-30 17:51:43 +0200

    druzynaOgniowa

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,7 +11,8 @@ module.exports = {
         -clear <ile> -> kasowanie wiadomości (max: 20 wiadomości, 14 dni wstecz)
         -sleep [użytkownicy] -> kick z głosowego
         -weapons -> meta broni Destiny 2
-        -server -> status serwera Destiny 2`);
+        -server -> status serwera Destiny 2
+        -druzynaOgniowa -> woła do gry REEE`);
     }
 
 }
