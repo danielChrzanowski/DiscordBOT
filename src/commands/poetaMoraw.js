49443572cb766491a48a4a80f0c030993a3c098a
Daniel <daniel_chrzanowski98@wp.pl>
2021-05-17 18:58:15 +0200

    poetaMoraw link

--- src/commands/poetaMoraw.js
+++ src/commands/poetaMoraw.js
@@ -7,7 +7,8 @@ module.exports = {
             '<:disaSmile:812821278984765490>',
         ];
 
-        const msg = await message.channel.send('**"Szlachetne zdrowie ee eeem prawda ile cię trzeba cenić czy jak a jak tak yy e m m jako smakujesz aż się zepsujesz prawda"** ~Jan Kochanowski');
+        const msg = await message.channel.send(`**"Szlachetne zdrowie ee eeem prawda ile cię trzeba cenić czy jak a jak tak yy e m m jako smakujesz aż się zepsujesz prawda"** ~Jan Kochanowski
+        https://www.youtube.com/watch?v=FHWbGkpveNk`);
 
         reactions.forEach(element => {
             msg.react(element);
