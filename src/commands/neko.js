829b02bc150ce98f322dce2772b00704747af686
Daniel <daniel_chrzanowski98@wp.pl>
2021-05-18 12:13:46 +0200

    check channel

--- src/commands/neko.js
+++ src/commands/neko.js
@@ -7,6 +7,16 @@ module.exports = {
     async execute(client, message, args) {
         if (!args.length) return message.reply('musisz podać agrument');
 
+        tags = ['4k', 'ass', 'blowjob', 'bj', 'boobs', 'cum', 'feet',
+            'hentai', 'wallpapers', 'spank', 'gasm', 'lesbian', 'lewd', 'pussy'];
+
+        for (i of tags) {
+            if (i == args[0]) {
+                if (!message.channel.nsfw)
+                    return message.reply('możesz użyć tego argumentu tylko na kanale NSFW');
+            }
+        }
+
         const { image } = await fetch('http://api.nekos.fun:8080/api/' + args[0])
             .then(response => response.json());
 
