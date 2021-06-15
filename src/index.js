3cb33f92dc56641ce222d3bf9fa108c1d3f4392a
Daniel <daniel_chrzanowski98@wp.pl>
2021-06-15 14:39:38 +0200

    weaver op fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ client.events = new Discord.Collection();
 
 client.on('ready', () => {
     console.log('Dzieci Neo is online!');
-    client.user.setActivity('Weaver OP', { type: 'WATCHING' });
+    client.user.setActivity('Weaver OP');
 });
 
 client.login(process.env.DISCORD_TOKEN);
