d61da797e6687461b702cf802a0455abafbc50a8
Daniel <daniel_chrzanowski98@wp.pl>
2021-07-19 18:50:58 +0200

    New CatApi images

--- src/commands/cat.js
+++ src/commands/cat.js
@@ -5,10 +5,17 @@ module.exports = {
     description: 'Prints random cat',
 
     async execute(client, message) {
+        /*
         const { file } = await fetch('https://aws.random.cat/meow')
             .then(response => response.json());
-
+        
         message.channel.send(file);
+        */
+
+        fetch('https://api.thecatapi.com/v1/images/search?format=json', {
+            headers: { 'x-api-key': process.env.DISCORD_TOKEN, }
+        }).then(function (response) { return response.json(); })
+            .then(data => { message.channel.send(data[0].url); });
     }
 
 }
