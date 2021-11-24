30cac39db133ee79ff8ddb757e9be4ae2aaeb868
Daniel <daniel_chrzanowski98@wp.pl>
2021-11-24 17:32:03 +0100

    fox API

---
+++ src/commands/fox.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const getRandom = require('../addons/random.js');
+const globalVariables = require('../addons/globalVariables.js');
+
+module.exports = {
+    name: 'fox',
+    description: 'Prints random fox',
+
+    async execute(client, message) {
+        const reactions = globalVariables.execute("cuteReactions");
+
+        try {
+            let url;
+            
+            await fetch('https://randomfox.ca/floof/')
+                .then(response => response.json())
+                .then(data => url = data.image);
+
+            const msg = await message.channel.send(url);
+
+            const i = getRandom.execute(0, reactions.length - 1);
+            msg.react(reactions[i]);
+        } catch (error) {
+            console.log(error);
+            client.channels.cache.get(process.env.LOG_CHANNEL_ID).send("--------------\nAPI foxika nie działa :(\n" + globalVariables.execute("currentDate"));
+            message.reply("nie ma foxika, bo API nie działa :(");
+        }
+    }
+}
