5e75e6872408ead07bc75d5096cf25b5c1559c53
Daniel <daniel_chrzanowski98@wp.pl>
2022-02-23 14:09:09 +0100

    try-catch

--- src/commands/dogeCounter.js
+++ src/commands/dogeCounter.js
@@ -8,9 +8,16 @@ module.exports = {
 
     async execute(client, message) {
         const reactions = globalVariables.execute("cuteReactions");
-       
-        const msg = await message.reply("Twój doge counter: " + await firebase.execute("getDogeCounter", message.author.id));
-        const i = getRandom.execute(0, reactions.length - 1);
-        msg.react(reactions[i]);
+
+        try {
+            const msg = await message.reply("Twój doge counter: " + await firebase.execute("getDogeCounter", message.author.id));
+            const i = getRandom.execute(0, reactions.length - 1);
+            msg.react(reactions[i]);
+
+        } catch (error) {
+            console.log(error);
+            client.channels.cache.get(process.env.LOG_CHANNEL_ID).send("--------------\nBaza danych firebase nie działa :(\n" + globalVariables.execute("currentDate"));
+            message.reply("nie ma, bo baza danych płonie :(");
+        }
     }
 }
