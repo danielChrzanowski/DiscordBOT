f3863732535cb6e52062c9c95774a6f4ef866479
Daniel <daniel_chrzanowski98@wp.pl>
2022-12-02 23:45:22 +0100

    Added express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const server = express();
+
+server.use((req, res) => {
+    res.send('Hello World!')
+});
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+    console.log('Server listening on http://localhost:' + PORT)
+});
+
+
 const Discord = require('discord.js');
 const dateBotName = require('./addons/dateBotName.js');
 const client = new Discord.Client();
