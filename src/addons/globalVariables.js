6a8dd9c273d7c7cc07af588d5cf1969fdc0b0de6
Daniel <daniel_chrzanowski98@wp.pl>
2023-05-08 20:25:11 +0200

    summer time fix

--- src/addons/globalVariables.js
+++ src/addons/globalVariables.js
@@ -55,7 +55,7 @@ module.exports = {
             const january = new Date(date_ob.getFullYear(), 0, 1).getTimezoneOffset();
             const july = new Date(date_ob.getFullYear(), 6, 1).getTimezoneOffset();
 
-            if (Math.max(january, july) !== date_ob.getTimezoneOffset()) date_ob.setHours(date_ob.getHours() + 1);
+            if (Math.max(january, july) !== date_ob.getTimezoneOffset()) date_ob.setHours(date_ob.getHours() + 2);
             return date_ob;
         }
     }
