ddf559c73e3a7325087a41d889183e917cd575bd
Daniel <daniel_chrzanowski98@wp.pl>
2025-06-27 21:44:56 +0200

    clean

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -17,18 +17,18 @@ export default {
     await interaction.deferReply();
     await interaction.editReply({
       content: `KOMENDY:
-            -cat -> losowy koteł
-            -clear <ile> -> kasowanie wiadomości (max: ${MAX_MESSAGES_TO_DELETE_COUNT} wiadomości, 14 dni wstecz)
-            -doge-counter [użytkownik] -> licznik piesełów (opcjonalnie można podać użytkownika do sprawdzenia)
-            -doge -> losowy pieseł
-            -druzyna-ogniowa -> woła Drużynę Ogniową REEE
-            -fun -> fun :)
-            -neko <argument> -> losowe neko
-            -ps2 [nick] -> populacja PS2 - Miller lub statystyki gracza (jeśli podasz nick w parametrze)
-            -summon-fox -> przyzywa Foxa
-            -summon <użytkownicy> -> przyzywa użytkowników (max ${MAX_USERS_TO_SUMMON_COUNT})
-            -sleep <użytkownicy> -> kick z głosowego (max ${MAX_USERS_TO_SLEEP_COUNT} użytkowników)
-            -wipe-time -> woła do BnS REEE`,
+      -cat -> losowy koteł
+      -clear <ile> -> kasowanie wiadomości (max: ${MAX_MESSAGES_TO_DELETE_COUNT} wiadomości, 14 dni wstecz)
+      -doge-counter [użytkownik] -> licznik piesełów (opcjonalnie można podać użytkownika do sprawdzenia)
+      -doge -> losowy pieseł
+      -druzyna-ogniowa -> woła Drużynę Ogniową REEE
+      -fun -> fun :)
+      -neko <argument> -> losowe neko
+      -ps2 [nick] -> populacja PS2 - Miller lub statystyki gracza (jeśli podasz nick w parametrze)
+      -summon-fox -> przyzywa Foxa
+      -summon <użytkownicy> -> przyzywa użytkowników (max ${MAX_USERS_TO_SUMMON_COUNT})
+      -sleep <użytkownicy> -> kick z głosowego (max ${MAX_USERS_TO_SLEEP_COUNT} użytkowników)
+      -wipe-time -> woła do BnS REEE`,
     });
   },
 };
