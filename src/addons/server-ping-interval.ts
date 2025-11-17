5f78c05fa93e4c91a6712f1fa9ecbb481ef86046
Daniel <daniel_chrzanowski98@wp.pl>
2025-11-17 20:13:11 +0100

    pinging interval delay

--- src/addons/server-ping-interval.ts
+++ src/addons/server-ping-interval.ts
@@ -2,12 +2,25 @@ export const initServerPingInterval = () => {
   const expressPort = process.env.EXPRESS_PORT || 3000;
   const serverPingUrl = process.env.SERVER_PING_URL || `http://localhost:${expressPort}/dummy`;
 
-  console.log(`Started pinging: ${serverPingUrl}`);
+  console.log(`Started ping server interval on: ${serverPingUrl}`);
 
-  setInterval(() => {
-    fetch(serverPingUrl)
-      .then((res) => res.json())
-      .then((data) => console.log(`GET /dummy:`, data))
-      .catch((err) => console.error(`Error fetching /dummy:`, err));
-  }, 2_000);
+  const serverStartingTimeoutTime = 60_000;
+  const interval = 2_000;
+
+  const ping = (delay: number) => {
+    setTimeout(() => {
+      fetch(serverPingUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(`GET /dummy:`, data);
+          ping(interval);
+        })
+        .catch((err) => {
+          console.error(`Error fetching /dummy:`, err);
+          ping(serverStartingTimeoutTime);
+        });
+    }, delay);
+  };
+
+  ping(serverStartingTimeoutTime);
 };
